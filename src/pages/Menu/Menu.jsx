972d79f1cf03dfc639995a6874d0b7e00377a236
saqib0404 <saqibahmad0404@gmail.com>
2025-05-19 08:29:34 +0600

    cover added

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,22 +1,23 @@
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
+import Cover from "../Shared/Cover";
+import menuCover from '../../assets/menu/banner3.jpg'
+import Testimonials from "../Home/Testimonials";
 
 const Menu = () => {
     const location = useLocation().pathname;
-
     // Remove the slash and capitalize the first letter
     let formattedPath = location.replace("/", "")  // Remove "/"
         .replace(/^./, c => c.toUpperCase()); // Capitalize first letter
-
 
     useEffect(() => {
         document.title = `Bistro | ${formattedPath}`
     }, [])
 
     return (
-        <div>
-            Menu
-        </div>
+        <>
+            <Cover img={menuCover} title={'OUR MENU'} />
+        </>
     )
 }
 
