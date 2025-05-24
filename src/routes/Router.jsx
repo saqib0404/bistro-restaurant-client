c112a2e6ca1a0320a89db88aa412279c430eb29a
saqib0404 <saqibahmad0404@gmail.com>
2025-05-24 11:15:50 +0600

    new layout setted

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,8 +5,11 @@ import Main from "../layout/Main";
 import Home from "../pages/Home/Home";
 import Menu from "../pages/Menu/Menu";
 import Order from "../pages/Order/Order";
+import Authentication from "../layout/Authentication";
+import Login from "../pages/Authentication/Login";
 
 export const router = createBrowserRouter([
+    // General Layout
     {
         path: "/",
         element: <Main />,
@@ -22,6 +25,17 @@ export const router = createBrowserRouter([
             {
                 path: 'order',
                 element: <Order />
+            },
+        ]
+    },
+    // Authentication Layout
+    {
+        path: '/authentication',
+        element: <Authentication />,
+        children: [
+            {
+                path: '/authentication/login',
+                element: <Login />
             },
         ]
     },
