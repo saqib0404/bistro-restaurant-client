3cd560c9fe060affeadf17ccae117a291693378c
saqib0404 <saqibahmad0404@gmail.com>
2025-06-09 14:36:27 +0600

    done

--- src/pages/Order/OrderCard.jsx
+++ src/pages/Order/OrderCard.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const OrderCard = ({ menus }) => {
+    
+    const handleAddToCart = food => {
+        console.log(food);
+    }
+
     return (
         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-2'>
             {
@@ -17,7 +22,9 @@ const OrderCard = ({ menus }) => {
                         <h2 className="text-2xl font-semibold text-center">{orderItem.name}</h2>
                         <p className='max-w-xs'>{orderItem.recipe}</p>
                         <div className="card-actions justify-center mt-2">
-                            <button className="btn btn-lg border-t-0 border-r-0 border-l-0 duration-200 hover:bg-[#111827] text-[#BB8406] border-b-2 border-[#BB8406] rounded-lg">ADD TO CART</button>
+                            <button
+                                onClick={() => handleAddToCart(orderItem)}
+                                className="btn btn-lg border-t-0 border-r-0 border-l-0 duration-200 hover:bg-[#111827] text-[#BB8406] border-b-2 border-[#BB8406] rounded-lg">ADD TO CART</button>
                         </div>
                     </div>
                 </div>)
