d51ac3c644e9502b1c603bf7ba955670e199f66f
saqib0404 <saqibahmad0404@gmail.com>
2025-06-10 11:52:23 +0600

    dashboard aside bar added

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,14 +1,27 @@
-import { FaCalendarAlt, FaHome, FaWallet } from "react-icons/fa"
+import { FaCalendarAlt, FaHome, FaShoppingBag, FaWallet } from "react-icons/fa"
 import { FaCartShopping } from "react-icons/fa6"
-import { MdReviews } from "react-icons/md"
+import { GiHamburgerMenu } from "react-icons/gi"
+import { MdMail, MdReviews } from "react-icons/md"
 import { RiReservedFill } from "react-icons/ri"
 import { NavLink, Outlet } from "react-router-dom"
 
 const Dashboard = () => {
     return (
-        <div className="flex">
-            <aside className="w-64 min-h-screen bg-[#D1A054] text-lg">
-                <ul>
+        <div className="drawer lg:drawer-open">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+
+            <div className="drawer-content flex flex-col items-center justify-center">
+                {/* Page content here */}
+                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
+                    Open Menu Bar
+                </label>
+                <Outlet />
+            </div>
+            =
+            <aside className="drawer-side bg-[#D1A054] text-xl">
+                <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="bg-[#D1A054] text-base-content space-y-2 min-h-full w-80 p-4">
+                    <h3 className="text-4xl text-center font-semibold py-4">Bistro Restaurant</h3>
                     <li className="flex items-center gap-2">
                         <FaHome /> <NavLink to='/dashboard/user-home'> USER HOME</NavLink>
                     </li>
@@ -24,15 +37,30 @@ const Dashboard = () => {
                     <li className="flex items-center gap-2">
                         <MdReviews /> <NavLink to='/dashboard/cart'> ADD REVIEW</NavLink>
                     </li>
-                    <li className="flex items-center gap-2">
+                    <li className="flex items-center gap-2 pb-5">
                         <RiReservedFill /> <NavLink to='/dashboard/cart'> MY BOOKING</NavLink>
+                    </li>
+
+                    <div className="w-11/12 border border-white border-opacity-80 mx-auto my-10"></div>
+
+                    <li className="flex items-center gap-2 pt-5">
+                        <FaHome /> <NavLink to='/'> HOME</NavLink>
+                    </li>
+                    <li className="flex items-center gap-2 ">
+                        <GiHamburgerMenu /> <NavLink to='/menu'> Menu</NavLink>
+                    </li>
+                    <li className="flex items-center gap-2 ">
+                        <FaShoppingBag /> <NavLink to='/order'> SHOP</NavLink>
+                    </li>
+                    <li className="flex items-center gap-2 ">
+                        <MdMail /> <NavLink to='/contacts'> CONTACT</NavLink>
                     </li>
                 </ul>
             </aside>
 
-            <div className="flex-1">
+            {/* <div className="flex-1">
                 <Outlet />
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -55,7 +55,7 @@ export const router = createBrowserRouter([
     // Dashboard Layout
     {
         path: '/dashboard',
-        element: <Dashboard />,
+        element: <PrivateRoute><Dashboard /></PrivateRoute>,
         children: [
             {
                 path: '/dashboard/cart',
